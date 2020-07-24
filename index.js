303b9e978b3734a19e8817827e9d7d3c13a76d10
quangnp <z.quang.nguyenphu@gree.net>
2020-07-24 17:25:13 +0700

    Ajax send token for authorization

--- index.js
+++ index.js
@@ -6,7 +6,8 @@ const cookieParser = require('cookie-parser');
 const accountRouter = require('./routers/account.js');
 const authRouter = require('./routers/auth.js');
 const userRouter = require('./routers/user.js');
-
+const jwt = require('jsonwebtoken');
+const AccountModel = require('./models/account.js');
 //CORS middleware
 app.use(function(req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
@@ -25,9 +26,53 @@ app.use('/public', express.static(path.join(__dirname, 'public')));
 // Config server port
 const port = 3000;
 
-app.get('/home', (req, res, next) => {
+app.get('/index', (req, res, next) => {
     res.sendFile(path.join(__dirname, 'index.html'))
 });
+
+app.get('/home', (req, res, next) => {
+    var token = req.cookies.token;
+    var decodeToken = jwt.verify(token, 'secret_password_here');
+    AccountModel.find({ _id: decodeToken._id})
+    .then(data => {
+        if (data.length == 0) {
+            return res.redirect("/login");
+        } else {
+            if (data[0].role == 2) {
+                next();
+            } else {
+                return res.redirect("/login");
+            }
+        }
+    })
+}, (req, res, next) => {
+    res.sendFile(path.join(__dirname, 'home.html'))
+});
+
+app.post("/edit", (req, res, next) => {
+    var token = req.headers.cookie.split("=")[1];
+    var decodeToken = jwt.verify(token, 'secret_password_here');
+    AccountModel.find({ _id: decodeToken._id})
+    .then(data => {
+        if (data.length == 0) {
+            return res.json({
+                error: true,
+                message: "NOT FOUND"
+            });
+        } else {
+            if (data[0].role == 2) {
+                next();
+            } else {
+                return res.json({
+                    error: true,
+                    message: "NOT PERMISSION"
+                });
+            }
+        }
+    })
+}, (req, res, next) => {
+    res.json('Edit ok');
+})
 
 // GET Login page
 app.get('/login', (req, res) => {
--- public/js/auth.js
+++ public/js/auth.js
@@ -10,15 +10,49 @@ function login() {
     })
     .then(data => {
         setCookie('token', data.token, 1);
+        window.location.href = "/home";
     })
     .catch(err => {
         console.log('API Error')
     })
 }
 
+$('#btnHomeEdit').click(function() {
+    var token = getCookie('token');
+    $.ajax({
+        url: '/edit',
+        type: 'POST',
+        headers: {
+            token: token
+        }
+    })
+    .then(data => {
+        console.log(data);
+    })
+    .catch(err => {
+        console.log('Server Error')
+    })
+});
+
 function setCookie(cname, cvalue, exdays) {
     var d = new Date();
     d.setTime(d.getTime() + (exdays*24*60*60*1000));
     var expires = "expires="+ d.toUTCString();
     document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+function getCookie(cname) {
+    var name = cname + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+    for(var i = 0; i <ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+        c = c.substring(1);
+    }
+        if (c.indexOf(name) == 0) {
+        return c.substring(name.length, c.length);
+    }
+    }
+    return "";
 }
